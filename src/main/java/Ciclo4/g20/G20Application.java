8f19b5d81c7f84442752366f32e5c840378aae5f
Nathaly Almario <almarionathaly@gmail.com>
2021-12-18 08:43:23 -0500

    method delete collections

--- src/main/java/Ciclo4/g20/G20Application.java
+++ src/main/java/Ciclo4/g20/G20Application.java
@@ -1,13 +1,34 @@
 package Ciclo4.g20;
 
+import Ciclo4.g20.interfaces.InterfaceOrder;
+import Ciclo4.g20.interfaces.InterfaceProduct;
+import Ciclo4.g20.interfaces.InterfaceUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication
-public class G20Application {
+public class G20Application implements CommandLineRunner {
+    
+        @Autowired
+        private InterfaceProduct interfaceProduct;
+        @Autowired
+        private InterfaceUser interfaceUser;
+        @Autowired
+        private InterfaceOrder interfaceOrder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(G20Application.class, args);
 	}
+        
+        @Override
+        public void run(String ... args) throws Exception{
+        interfaceProduct.deleteAll();
+        interfaceUser.deleteAll();
+        interfaceOrder.deleteAll();
+        }
 
 }
